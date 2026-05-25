ddadfc42578981e1bc233324a92cbf080e0a08cb
Murat Akkus <akkusmurat123@gmail.com>
2026-05-25 02:29:03 +0200

    feat: Add Supertest API Testing to project

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,4 +1,6 @@
 import { expect, test } from "vitest";
+import request from "supertest";
+import { app } from "./app.js";
 
 const sum = (a: number, b: number) => {
   return a + b;
@@ -7,3 +9,9 @@ const sum = (a: number, b: number) => {
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+request(app)
+  .get("/")
+  .then((res) => {
+    expect(res.text).toBe("Hello World!");
+  });
