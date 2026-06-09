5dc445e3b2b83884d8c6c98c1654157e7c18a165
Murat Akkus <akkusmurat123@gmail.com>
2026-06-10 00:10:53 +0200

    feat(server): add health endpoint and json body parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from "express";
 const app = express();
 
+app.use(express.json({ limit: "100kb" }));
+
+app.get("/health", (_req, res) => {
+  res.send("ok").status(200);
+});
+
 app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
