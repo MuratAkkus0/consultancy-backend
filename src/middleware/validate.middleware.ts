9741c3c40ee36d919d0b9b414604b8e10c2094be
Murat Akkus <akkusmurat123@gmail.com>
2026-07-21 14:53:25 +0200

    feat: Add validateBodyByRole middleware

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -1,9 +1,34 @@
 import type { Request, Response, NextFunction } from "express";
 import type { ZodType } from "zod";
 import createHttpError from "http-errors";
+import type { UserRole } from "../db/index.js";
 
 export const validateBody = (schema: ZodType) => {
   return (req: Request, _res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      throw createHttpError(400, "Validation failed", {
+        issues: result.error.issues,
+      });
+    }
+    // Change request body with parsed and cleaned data
+    req.body = result.data;
+    next();
+  };
+};
+
+export const validateBodyByRole = (
+  schemas: Partial<Record<UserRole, ZodType>>,
+) => {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const role = req.user!.role as UserRole;
+
+    const schema = role ? schemas[role] : undefined;
+
+    if (!schema) {
+      throw createHttpError(403, "This role cannot edit its profile.");
+    }
+
     const result = schema.safeParse(req.body);
     if (!result.success) {
       throw createHttpError(400, "Validation failed", {
