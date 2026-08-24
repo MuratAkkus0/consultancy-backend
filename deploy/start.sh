047ba1f63b32b2d027f3b19114392bb878c22fd2
Murat Akkus <akkusmurat123@gmail.com>
2026-08-24 14:32:07 +0200

    chore: add nginx configurations

--- deploy/start.sh
+++ deploy/start.sh
@@ -12,4 +12,13 @@ ENV_EXPORTS="$(
 )"
 eval "$ENV_EXPORTS"
 
+# configure nginx
+# copy upgrade.conf
+cp /home/admin/consultancy-backend/deploy/nginx/conf.d/upgrade.conf /etc/nginx/conf.d/upgrade.conf
+# copy milestone_backend
+cp /home/admin/consultancy-backend/deploy/nginx/sites-available/milestone_backend /etc/nginx/sites-available/milestone_backend
+
+sudo nginx -t && sudo systemctl reload nginx.service
+
+
 exec /usr/bin/node /home/admin/consultancy-backend/dist/server.js
